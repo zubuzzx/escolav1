7c22562508a82f7bdd392f0d6ce56b6c1e3cab67
gustavo <gustavo767psal@gmail.com>
2024-02-27 15:15:35 -0300

    Front end inicial

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+    <title>Escola</title>
+  </head>
+
+  <body>
+    <?php
+        include 'menu.html';
+    ?> 
+
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col">            
+            <img class="img-fluid" src="./assets/escola1.jpg" alt="" srcset="">
+        </div>
+        <div class="col-4"></div>
+    </div>
+
+
+
+  
+   
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
